8c557c09857f7f010c65bd5142e8386725defad4
Benito <benito.pacheco@citi.com.mx>
2018-09-06 10:40:41 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Proyecto Rest Server
+
+Ejecutar el siguiente comando para las librearias de node
+
+```
+npm install 
+```
---
+++ server/config/config.js
@@ -0,0 +1,4 @@
+//=====================================
+// Puerto. propiedad global
+//=====================================
+process.env.PORT = process.env.PORT || 3000
